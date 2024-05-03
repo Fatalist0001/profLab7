06ebb0b77b47cd0ddefd76f89913ecea9d1cf89a
Fatalit0001 <odinodinodin640@gmail.com>
2024-05-03 08:54:47 +0300

    add class JsonService

--- include/Service.h
+++ include/Service.h
@@ -7,3 +7,9 @@ class Service{
     virtual Service getWeather(std::string s) = 0;
     virtual ~Service();
 };
+
+class JsonService :public Service{
+public:
+	virtual Weather getWeather(string s) override;
+	virtual ~JsonService() {};
+};
